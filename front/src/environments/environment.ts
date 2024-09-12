dae02c403dc848d5021d4b0de51f1535f891cb0f
Mohamed Coulibaly <mcouliba985@gmail.com>
2024-09-12 22:14:49 +0000

    config finish

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  //apiUrl: 'https://mafa.magservices-mali.org/api'
-  apiUrl: 'http://127.0.0.1:8000/api'
+  apiUrl: 'https://mafa.magservices-mali.org/api'
+  // apiUrl: 'http://127.0.0.1:8000/api'
 };
